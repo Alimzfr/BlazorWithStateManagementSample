cf96ddae808a0cb3d84b1f298e529c825a76c893
AliMozaffari <a-mozafari@agi-bank.com>
2024-05-21 10:15:59 +0330

    Add project files.

---
+++ BlazorWithStateManagementSample/BlazorWithStateManagementSample/Program.cs
@@ -0,0 +1,35 @@
+using BlazorWithStateManagementSample.Client.Pages;
+using BlazorWithStateManagementSample.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(BlazorWithStateManagementSample.Client._Imports).Assembly);
+
+app.Run();
